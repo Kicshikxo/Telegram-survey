a309f58f3a007d90524b7690f38193c597fd3203
Kicshikxo <supermegavalera9000@gmail.com>
2023-04-26 01:51:46 +0300

    Remove unused endpoints

--- server/api/telegram/broadcast.ts
+++
@@ -1,9 +0,0 @@
-
-export default defineEventHandler(async event => {
-    const tokenData = readTokenData(event)
-    if (!tokenData) return
-
-    const query = getQuery(event) as { text: string }
-
-    return { success: true }
-})
