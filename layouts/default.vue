5ab70e0bb45e3de3a63272d5c005cac699c66251
Kicshikxo <supermegavalera9000@gmail.com>
2023-04-26 01:51:18 +0300

    Add ConfirmDialog

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,12 @@
 <template>
     <main>
+        <ConfirmDialog />
         <Toast />
         <slot />
     </main>
 </template>
 
 <script setup lang="ts">
+import ConfirmDialog from 'primevue/confirmdialog'
 import Toast from 'primevue/toast'
 </script>
