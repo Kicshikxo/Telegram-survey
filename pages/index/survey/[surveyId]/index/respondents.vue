7b5b1ac7c3df8c56321e6b3884da16540dec1b4b
Kicshikxo <supermegavalera9000@gmail.com>
2023-05-03 02:13:36 +0300

    Add respondent assesment template

--- pages/index/survey/[surveyId]/index/respondents.vue
+++ pages/index/survey/[surveyId]/index/respondents.vue
@@ -1,22 +1,28 @@
 <template>
     <div>
-        <DataTable :value="survey?.respondents" class="p-datatable-lg">
+        <DataTable :value="survey?.respondents" removableSort class="p-datatable-lg">
             <template #header>
                 <div class="flex justify-content-end">
                     <Button label="Обновить" icon="pi pi-refresh" :loading="loadingSurvey" @click="() => refreshSurvey()" />
                 </div>
             </template>
-            <Column field="secondName" header="Фамилия" />
-            <Column field="firstName" header="Имя" />
-            <Column field="middleName" header="Отчество" />
+            <Column field="secondName" header="Фамилия" sortable />
+            <Column field="firstName" header="Имя" sortable />
+            <Column field="middleName" header="Отчество" sortable />
+            <Column field="middleName" header="Оценка">
+                <template #body="{ data }">
+                    <Dropdown :options="[2, 3, 4, 5]" placeholder="Выберите оценку" />
+                </template>
+            </Column>
         </DataTable>
     </div>
 </template>
 
 <script setup lang="ts">
 import Button from 'primevue/button'
-import Column from 'primevue/column'
-import DataTable from 'primevue/datatable'
+import Column from 'primevue/column/Column.vue'
+import DataTable from 'primevue/datatable/DataTable.vue'
+import Dropdown from 'primevue/dropdown/Dropdown.vue'
 
 const route = useRoute()
 
