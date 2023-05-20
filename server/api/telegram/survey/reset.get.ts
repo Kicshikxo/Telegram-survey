75abbd07ca144bf4912f39a72783ff1ae8d5664a
Kicshikxo <supermegavalera9000@gmail.com>
2023-05-20 23:35:11 +0300

    Add reset survey endpoint

---
+++ server/api/telegram/survey/reset.get.ts
@@ -0,0 +1,16 @@
+import { SurveyStatus } from "@prisma/client"
+
+export default defineEventHandler(async event => {
+    const tokenData = readTokenData(event)
+    if (!tokenData) return
+
+    const query = getQuery(event) as { surveyId: string }
+    if (!query.surveyId) return sendError(event, createError({ statusCode: 400, statusMessage: 'surveyId is not provided' }))
+
+    const survey = await prisma.survey.findUnique({ where: { id: query.surveyId } })
+    if (!survey) return
+
+    await prisma.survey.update({ where: { id: survey.id }, data: { status: SurveyStatus.IN_DEVELOPMENT } })
+
+    return { success: true }
+})
