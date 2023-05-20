278f0827ffc6d880e4bd76dc3793620952b944d9
Kicshikxo <supermegavalera9000@gmail.com>
2023-05-20 23:34:09 +0300

    Add IN_DEVELOPMENT status localization

--- utils/localizations.ts
+++ utils/localizations.ts
@@ -1,6 +1,7 @@
 import { SurveyStatus } from "@prisma/client";
 
 export const localizedSurveyStatus: { [key in SurveyStatus]: string } = {
+    IN_DEVELOPMENT: 'В разработке',
     NOT_STARTED: 'Не начато',
     IN_PROGRESS: 'В процессе',
     FINISHED: 'Завершено'
