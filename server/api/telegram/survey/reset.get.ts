44df2df3d13bda9f799544b7aa70427af1ed4c6a
Kicshikxo <supermegavalera9000@gmail.com>
2023-05-21 00:26:25 +0300

    Reset respondents list on survey reset

--- server/api/telegram/survey/reset.get.ts
+++ server/api/telegram/survey/reset.get.ts
@@ -10,7 +10,7 @@ export default defineEventHandler(async event => {
     const survey = await prisma.survey.findUnique({ where: { id: query.surveyId } })
     if (!survey) return
 
-    await prisma.survey.update({ where: { id: survey.id }, data: { status: SurveyStatus.IN_DEVELOPMENT } })
+    await prisma.survey.update({ where: { id: survey.id }, data: { status: SurveyStatus.IN_DEVELOPMENT, respondents: { set: [] } } })
 
     return { success: true }
 })
